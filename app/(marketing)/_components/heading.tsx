4ae183c1e230d6a716fa47d4f55d0896fa20c759
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2024-02-28 03:42:57 -0800

    Initial commit: Started development of the Notes application using Next.js.

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+
+import React from 'react';
+import {Button} from "@/components/ui/button";
+import {ArrowRight} from "lucide-react";
+import {useConvex, useConvexAuth} from "convex/react";
+import {Spinner} from "@/components/spinner";
+
+const Heading = () => {
+
+    const {isAuthenticated, isLoading} = useConvexAuth()
+
+    return (
+        <div className={'max-w-3xl space-y-4'}>
+            <h1 className={'text-3xl sm:text-5xl md:text-6xl font-bold'}>
+                Your Ideas,Documents, & Plans. Unifed.Welcome to <span className={'underline'}>
+                Jotion
+            </span>
+            </h1>
+            <h3 className={'text-base sm:text-xl md:text-2xl font-medium'}>
+                Jotion is the connected workspace where <br/>
+                better, faster work happens.
+            </h3>
+            {isLoading && (
+                <Spinner size={'lg'} />
+            )}
+            {isAuthenticated && !isLoading && (
+            <Button>
+                Enter Jotion
+                <ArrowRight className={'h-4 w-4 ml-2'}/>
+            </Button>
+            )}
+        </div>
+    );
+};
+
+export default Heading;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MarketingPage from "@/app/(marketing)/page";
+import MarketingLayout from "@/app/(marketing)/layout";
+
+export default function Home() {
+    return (
+        <div>
+            <MarketingLayout><MarketingPage/></MarketingLayout>
+
+        </div>);
+}
